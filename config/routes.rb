f14b7dda8795965775574ed3124b7b0cf8d84418
Caroline Courtney <caroline@carolinecourtney.com>
2016-04-06 02:37:05 -0400

    User Profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  # Users and profiles
   devise_for :users
+  match 'users/:id' => 'users#show', :as => :profile, via: :get
+
   get 'welcome/index'
   get 'welcome/about'
 
