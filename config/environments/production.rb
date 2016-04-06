f8e9d09d035642fe626b0bf17e3d021187987d2b
Caroline Courtney <caroline@carolinecourtney.com>
2016-04-06 01:59:38 -0400

    Confirmation tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,16 +15,16 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.action_mailer.default_url_options = {:host => 'https://mighty-eyrie-26757.herokuapp.com/'}
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-     :tls => true,
-     :address => "smtp.gmail.com",
-     :port => 587,
-     :domain => "gmail.com",
-     :authentication => :login,
-     :user_name => ENV["confirmation_email"],
-     :password => ENV["confirmation_password"]
-   }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    user_name: ENV['confirmation_email'],
+    password: ENV['confirmation_password'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
