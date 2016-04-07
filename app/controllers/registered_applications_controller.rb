3b0d473199e8bb0a65d320a9cd873cc65418784b
Caroline Courtney <caroline@carolinecourtney.com>
2016-04-07 02:38:24 -0400

    Create and show registered applications

--- app/controllers/registered_applications_controller.rb
+++ app/controllers/registered_applications_controller.rb
@@ -4,4 +4,30 @@ class RegisteredApplicationsController < ApplicationController
     @registered_applications = RegisteredApplication.all
   end
 
+  def show
+    @registered_application = RegisteredApplication.find(params[:id])
+  end
+
+  def new
+    @registered_application = RegisteredApplication.new
+  end
+
+  def create
+    @registered_application = RegisteredApplication.new(regapp_params)
+    @registered_application.user = current_user
+    if @registered_application.save
+      flash[:notice] = "App '#{@registered_application.name}' was saved."
+      redirect_to @registered_application
+    else
+      flash.now[:alert] = "There was an error registering this app.  Please try again."
+      render :new
+    end
+  end
+
+  private
+
+  def regapp_params
+    params.require(:registered_application).permit(:name, :url)
+  end
+
 end
--- spec/controllers/registered_applications_controller_spec.rb
+++ spec/controllers/registered_applications_controller_spec.rb
@@ -5,41 +5,58 @@ RSpec.describe RegisteredApplicationsController, type: :controller do
   let(:my_user) { create(:user) }
   let(:my_app) { create(:registered_application, user: my_user) }
 
-  describe "GET #index" do
-    it "renders the index template" do
-      get :index
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template("index")
-    end
-
-    it "assigns [my_app] to @registered_applications" do
-      get :index
-      expect(assigns(:registered_applications)).to eq(my_app)
-    end
+  context "guest user" do
   end
 
-  # describe "GET #show" do
-  #   it "renders the show template" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #     expect(response).to render_template("show")
-  #   end
-  # end
-  #
-  # describe "GET #new" do
-  #   it "renders the new template" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #     expect(response).to render_template("new")
-  #   end
-  # end
-  #
-  # describe "GET #edit" do
-  #   it "renders the edit template" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #     expect(response).to render_template("edit")
-  #   end
-  # end
+  context "signed in user" do
+    before(:each) do
+      request.env["HTTP_REFERER"] = "where_i_came_from"
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in my_user
+    end
 
+    describe "GET #index" do
+      it "renders the index template" do
+        get :index
+        expect(response).to render_template("index")
+      end
+
+      it "assigns [my_app] to @registered_applications" do
+        get :index
+        expect(assigns(:registered_applications)).to eq([my_app])
+      end
+    end
+
+    describe "GET #show" do
+      it "assigns my_app to @registered_application" do
+        get :show, { id: my_app.id }
+        expect(assigns(:registered_application)).to eq(my_app)
+      end
+    end
+
+    describe "GET #new" do
+      it "renders the new template" do
+        get :new
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template("new")
+      end
+
+      it "increases the number of apps by 1" do
+        expect{ create(:registered_application, user: my_user) }.to change(RegisteredApplication,:count).by(1)
+      end
+
+      it "redirects to the new app's page" do
+        post :create, registered_application: {name: "name", url: "url", user: my_user}
+        expect(response).to redirect_to RegisteredApplication.last
+      end
+    end
+
+    # describe "GET #edit" do
+    #   it "renders the edit template" do
+    #     get :show
+    #     expect(response).to have_http_status(:success)
+    #     expect(response).to render_template("edit")
+    #   end
+    # end
+  end
 end
